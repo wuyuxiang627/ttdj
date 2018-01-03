e5e9a4f950a5fb1ba3a05379d4b17f7e7281af3d
wuyuxiang <wuyuxiang_627@163.com>
2018-01-03 10:34:03 +0800

    全部分类-一级菜单

--- app/src/main/java/com/connxun/ttdj/ui/allClassification/AllClassificationActivity.java
+++ app/src/main/java/com/connxun/ttdj/ui/allClassification/AllClassificationActivity.java
@@ -1,11 +1,14 @@
 package com.connxun.ttdj.ui.allClassification;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ImageView;
 import android.widget.ListView;
 
+import com.connxun.ttdj.MyApplication;
 import com.connxun.ttdj.R;
 import com.connxun.ttdj.entity.CategoryMenu;
 import com.connxun.ttdj.entity.CategorySub;
@@ -21,6 +24,7 @@ import java.util.List;
 import javax.inject.Inject;
 
 import butterknife.BindView;
+import butterknife.OnClick;
 
 /**
  * Created by connxun-16 on 2018/1/2.
@@ -33,6 +37,9 @@ public class AllClassificationActivity extends BaseSwipeBackActivity implements 
 
     @BindView(R.id.lv_all_classification_listview)
     ListView lvAllClassification; //标题栏
+
+    @BindView(R.id.iv_all_classification_finish)
+    ImageView ivFinish; //销毁
 
 
     @Inject
@@ -50,7 +57,7 @@ public class AllClassificationActivity extends BaseSwipeBackActivity implements 
 
     @Override
     public void onError(String error) {
-
+        Toast(error);
     }
 
     @Override
@@ -62,6 +69,7 @@ public class AllClassificationActivity extends BaseSwipeBackActivity implements 
         lvAllClassification.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                presenter.getCategorySubList(carouseMenus.get(position).getCategoryid());
                 myAllClassificationAdapter.setSelectedPosition(position);
                 myAllClassificationAdapter.notifyDataSetChanged();
             }
@@ -115,4 +123,17 @@ public class AllClassificationActivity extends BaseSwipeBackActivity implements 
     public void onValidationFailed(List<ValidationError> errors) {
 
     }
+
+    /**点击事件*/
+    @OnClick({R.id.iv_all_classification_finish})
+    public void onViewClicked(View view) {
+        Intent intent = null;
+        switch (view.getId()) {
+            case R.id.iv_all_classification_finish:
+                //返回
+                this.finish();
+                break;
+        }
+    }
+
 }
